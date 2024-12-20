309dfde5fdf185c0123a44198d85d07830cfae36
AlyssonMts <alyssonaraujo767@gmail.com>
2024-12-20 01:14:22 -0300

    commit 1 com criação das pastas

---
+++ app.js
@@ -0,0 +1,2 @@
+const express = require("express");
+const connecttomongo = require("./db");
---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+  mongoose
+    .connect(
+      
+    )
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
+};
+
+module.exports = connectToDb;
